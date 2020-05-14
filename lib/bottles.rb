ed7c03b15d0ceb6afe2511cce5b5347163c6dc45
Sandi Metz <sandi@sandimetz.com>
2020-05-14 16:36:49 -0400

    make phrase 1/2 of case 1/else identical

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -16,12 +16,12 @@ class Bottles
       "99 bottles of milk on the wall.\n"
     when 1
       "#{number} #{container(number)} of milk on the wall, " +
-      "1 bottle of milk.\n" +
+      "#{number} #{container(number)} of milk.\n" +
       "Take it down and pass it around, " +
       "no more bottles of milk on the wall.\n"
     else
-      "#{number} bottles of milk on the wall, " +
-      "#{number} bottles of milk.\n" +
+      "#{number} #{container(number)} of milk on the wall, " +
+      "#{number} #{container(number)} of milk.\n" +
       "Take one down and pass it around, " +
       "#{number-1} #{container(number-1)} of milk on the wall.\n"
     end
