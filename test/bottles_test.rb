3044707685dc4481e82f9cd8a82bcf128cf1f54c
Sandi Metz <sandi@sandimetz.com>
2020-05-14 11:24:18 -0400

    verse 99 test

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -4,4 +4,12 @@ require 'minitest/pride'
 require_relative '../lib/bottles'
 
 class BottlesTest < Minitest::Test
+  def test_the_first_verse
+    expected =
+      "99 bottles of milk on the wall, " +
+      "99 bottles of milk.\n" +
+      "Take one down and pass it around, " +
+      "98 bottles of milk on the wall.\n"
+    assert_equal expected, Bottles.new.verse(99)
+  end
 end
