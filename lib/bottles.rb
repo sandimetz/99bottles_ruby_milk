d5750417300b22f6a82fb94acd89a64b3baf7316
Sandi Metz <sandi@sandimetz.com>
2020-05-16 10:39:16 -0400

    use result of sending #lyrics

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -9,13 +9,6 @@ class Bottles
 
   def verse(number)
     BottleVerse.new(number).lyrics
-
-    bottle_number = BottleNumber.for(number)
-
-    "#{bottle_number} of milk on the wall, ".capitalize +
-    "#{bottle_number} of milk.\n" +
-    "#{bottle_number.action}, " +
-    "#{bottle_number.successor} of milk on the wall.\n"
   end
 end
 
