751a4c576e7390d6686a7f9aaccb10ed231bd13d
Sandi Metz <sandi@sandimetz.com>
2020-05-16 07:07:27 -0400

    add six-pack to song test

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -361,9 +361,9 @@ class BottlesTest < Minitest::Test
       Take one down and pass it around, 7 bottles of milk on the wall.
 
       7 bottles of milk on the wall, 7 bottles of milk.
-      Take one down and pass it around, 6 bottles of milk on the wall.
+      Take one down and pass it around, 1 six-pack of milk on the wall.
 
-      6 bottles of milk on the wall, 6 bottles of milk.
+      1 six-pack of milk on the wall, 1 six-pack of milk.
       Take one down and pass it around, 5 bottles of milk on the wall.
 
       5 bottles of milk on the wall, 5 bottles of milk.
