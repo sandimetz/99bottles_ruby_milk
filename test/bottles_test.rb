7454150f3fcc2e6ec54cbc5f51a16f5a98def43a
Sandi Metz <sandi@sandimetz.com>
2020-05-18 08:30:47 -0400

    delete redundant test_a_few_verses

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -88,25 +88,6 @@ class CountdownSongTest < Minitest::Test
         .verses(99, 97))
   end
 
-  def test_a_few_verses
-    expected =
-      "2 bottles of milk on the wall, " +
-      "2 bottles of milk.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of milk on the wall.\n" +
-      "\n" +
-      "1 bottle of milk on the wall, " +
-      "1 bottle of milk.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of milk on the wall.\n" +
-      "\n" +
-      "No more bottles of milk on the wall, " +
-      "no more bottles of milk.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of milk on the wall.\n"
-    assert_equal expected, CountdownSong.new.verses(2, 0)
-  end
-
   def test_the_whole_song
     expected = <<~SONG
       99 bottles of milk on the wall, 99 bottles of milk.
