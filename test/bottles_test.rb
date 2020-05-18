64fe9ad0ae828e7599e16186e796175d14e58345
Sandi Metz <sandi@sandimetz.com>
2020-05-18 08:03:51 -0400

    delete obsolete test

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -51,42 +51,6 @@ class BottleVerseTest < Minitest::Test
 end
 
 class BottlesTest < Minitest::Test
-  def test_another_verse
-    expected =
-      "3 bottles of milk on the wall, " +
-      "3 bottles of milk.\n" +
-      "Take one down and pass it around, " +
-      "2 bottles of milk on the wall.\n"
-    assert_equal expected, Bottles.new.verse(3)
-  end
-
-  def test_verse_2
-    expected =
-      "2 bottles of milk on the wall, " +
-      "2 bottles of milk.\n" +
-      "Take one down and pass it around, " +
-      "1 bottle of milk on the wall.\n"
-    assert_equal expected, Bottles.new.verse(2)
-  end
-
-  def test_verse_1
-    expected =
-      "1 bottle of milk on the wall, " +
-      "1 bottle of milk.\n" +
-      "Take it down and pass it around, " +
-      "no more bottles of milk on the wall.\n"
-     assert_equal expected, Bottles.new.verse(1)
-  end
-
-  def test_verse_0
-    expected =
-      "No more bottles of milk on the wall, " +
-      "no more bottles of milk.\n" +
-      "Go to the store and buy some more, " +
-      "99 bottles of milk on the wall.\n"
-    assert_equal expected, Bottles.new.verse(0)
-  end
-
   def test_a_couple_verses
     expected =
       "99 bottles of milk on the wall, " +
